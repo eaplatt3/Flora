8f790425be33fb9e9deda36cfae49994f26f0983
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-12-04 15:22:48 -0500

    GiveAway Commands

---
+++ Flora/Modules/GiveAway.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flora.Modules
+{
+    class GiveAway : ModuleBase
+    {
+        public List<GiveAwayValues> initValues { get; private set; }
+
+        static GiveAway()
+        {
+            initValues = new List<GiveAwayValues>();
+        }
+
+        private Flora.Interfaces.IDiscordClient _discordClient;
+        private GiveAwayFunctions _functions;
+    }
+}
